a91b563123950b585dbb2b711c019363d21a60c4
Jake Walker <jakewalk93@hotmail.com>
2025-06-19 20:37:30 +0100

    removing shite

--- build/lib/core/init.py
+++
@@ -1,4 +0,0 @@
-from src.db.wallet_db import init_wallet_table
-
-def init_app():
-    init_wallet_table()
--- build/lib/models/wallet.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Dict
-
-class Wallet(BaseModel):
-    holdings: Dict[str, float]
-    pln_holdings: Dict[str, float]
-    total_pln: float
