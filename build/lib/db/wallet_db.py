2a814084607ea3656e73441dcca3f7361a6d042c
Jake <jakewalk93@hotmail.com>
2025-06-19 10:16:35 +0100

    Testing again

--- build/lib/db/wallet_db.py
+++ build/lib/db/wallet_db.py
@@ -28,3 +28,53 @@ def init_wallet_table():
         if count == 0:
             cursor.execute('INSERT INTO wallet (currency, amount) VALUES (?, ?)', ('PLN', 0.0))
             conn.commit()
+
+def add_currency_amount(currency: str, amount: float):
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "UPDATE wallet SET amount = amount + ? WHERE currency = ?",
+            (amount, currency)
+        )
+
+        if cursor.rowcount == 0:
+            cursor.execute(
+                "INSERT INTO wallet (currency, amount) VALUES (?, ?)",
+                (currency, amount)
+            )
+
+        conn.commit()
+    conn.close()
+
+def subtract_currency_amount(currency: str, amount: float):
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT amount FROM wallet WHERE currency = ?", (currency,))
+        row = cursor.fetchone()
+        if row is None:
+            raise ValueError(f"Currency '{currency}' not found in wallet.")
+
+        current_amount = row[0]
+
+        if amount > current_amount:
+            raise ValueError(f"Insufficient funds in {currency}.")
+
+        cursor.execute(
+            "UPDATE wallet SET amount = amount - ? WHERE currency = ?",
+            (amount, currency)
+        )
+
+    conn.commit()
+
+def remove_currency(currency: str):
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "DELETE FROM wallet WHERE currency = ?",
+            (currency,)
+        )
+        if cursor.rowcount == 0:
+            raise ValueError(f"Currency '{currency}' does not exist in the wallet.")
+    conn.commit()
--- build/lib/models/wallet.py
+++ build/lib/models/wallet.py
@@ -3,7 +3,5 @@ from typing import Dict
 
 class Wallet(BaseModel):
     holdings: Dict[str, float]
-
-    @property
-    def pln_total(self) -> float:
-        return self.holdings.get("PLN", 0.0)
+    pln_holdings: Dict[str, float]
+    total_pln: float
